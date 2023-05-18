0677204f00eaa54f63d70aacc1081b2c0ca6b6b7
McMeck Nechbech <bimpmpbem@gmail.com>
2023-05-18 16:53:18 +0300

    Add SyncedPlayerControllerGroup and common interfaces
    GenericPlayerController
    GenericPlayerValue
    ObstructionBehavior

---
+++ lib/synced_player.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/foundation.dart';
+
+/// The duration, current position, buffering state, error state and settings
+/// of a [GenericPlayerController].
+// Copied from video_player, and modified to be more generic
+// (w/o audio, captions, etc.)
+@immutable
+class GenericPlayerValue {
+  /// Constructs with the given values. Only [duration] is required. The
+  /// rest will initialize with default values when unset.
+  const GenericPlayerValue({
+    required this.duration,
+    this.position = Duration.zero,
+    this.isInitialized = false,
+    this.isPlaying = false,
+    this.isLooping = false,
+    this.isBuffering = false,
+    this.playbackSpeed = 1.0,
+    this.errorDescription,
+  });
+
+  /// Returns an instance for content that hasn't been loaded.
+  const GenericPlayerValue.uninitialized()
+      : this(duration: Duration.zero, isInitialized: false);
+
+  /// Returns an instance with the given [errorDescription].
+  const GenericPlayerValue.erroneous(String errorDescription)
+      : this(
+            duration: Duration.zero,
+            isInitialized: false,
+            errorDescription: errorDescription);
+
+  /// This constant is just to indicate that parameter is not passed to [copyWith]
+  /// workaround for this issue https://github.com/dart-lang/language/issues/2009
+  static const String _defaultErrorDescription = 'defaultErrorDescription';
+
+  /// The current total duration of the content.
+  ///
+  /// The duration is [Duration.zero] if the content hasn't been initialized.
+  ///
+  /// Note that the duration is not necessarily constant and
+  /// might change during the life of the content (for example, livestreams)
+  final Duration duration;
+
+  /// The current playback position.
+  final Duration position;
+
+  /// True if the content is playing. False if it's paused.
+  final bool isPlaying;
+
+  /// True if the content is looping.
+  final bool isLooping;
+
+  /// True if the content is currently buffering.
+  final bool isBuffering;
+
+  /// The current speed of the playback.
+  final double playbackSpeed;
+
+  /// A description of the error if present.
+  ///
+  /// If [hasError] is false this is `null`.
+  final String? errorDescription;
+
+  /// Indicates whether or not the content has been loaded and is ready to play.
+  final bool isInitialized;
+
+  /// Indicates whether or not the content is in an error state. If this is true
+  /// [errorDescription] should have information about the problem.
+  bool get hasError => errorDescription != null;
+
+  /// Returns a new instance that has the same values as this current instance,
+  /// except for any overrides passed in as arguments to [copyWith].
+  GenericPlayerValue copyWith({
+    Duration? duration,
+    Duration? syncOffset,
+    Duration? position,
+    bool? isInitialized,
+    bool? isPlaying,
+    bool? isLooping,
+    bool? isBuffering,
+    double? playbackSpeed,
+    int? rotationCorrection,
+    String? errorDescription = _defaultErrorDescription,
+  }) {
+    return GenericPlayerValue(
+      duration: duration ?? this.duration,
+      position: position ?? this.position,
+      isInitialized: isInitialized ?? this.isInitialized,
+      isPlaying: isPlaying ?? this.isPlaying,
+      isLooping: isLooping ?? this.isLooping,
+      isBuffering: isBuffering ?? this.isBuffering,
+      playbackSpeed: playbackSpeed ?? this.playbackSpeed,
+      errorDescription: errorDescription != _defaultErrorDescription
+          ? errorDescription
+          : this.errorDescription,
+    );
+  }
+
+  @override
+  String toString() {
+    return '${objectRuntimeType(this, 'VideoPlayerValue')}('
+        'duration: $duration, '
+        'position: $position, '
+        'isInitialized: $isInitialized, '
+        'isPlaying: $isPlaying, '
+        'isLooping: $isLooping, '
+        'isBuffering: $isBuffering, '
+        'playbackSpeed: $playbackSpeed, '
+        'errorDescription: $errorDescription)';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is GenericPlayerValue &&
+          runtimeType == other.runtimeType &&
+          duration == other.duration &&
+          position == other.position &&
+          isPlaying == other.isPlaying &&
+          isLooping == other.isLooping &&
+          isBuffering == other.isBuffering &&
+          playbackSpeed == other.playbackSpeed &&
+          errorDescription == other.errorDescription &&
+          isInitialized == other.isInitialized;
+
+  @override
+  int get hashCode => Object.hash(
+        duration,
+        position,
+        isPlaying,
+        isLooping,
+        isBuffering,
+        playbackSpeed,
+        errorDescription,
+        isInitialized,
+      );
+}
+
+/// Types of behavior a [GenericPlayerController] can choose when encountering
+/// an obstruction.
+enum ObstructionBehavior {
+  /// Keep playing normally.
+  none,
+
+  /// Attempt to reduce resource usage as much as possible.
+  // TODO maybe remove this
+  optimize,
+
+  /// Pause playback until obstruction is removed.
+  pauseUntilRemoved,
+
+  /// Pause playback until manual play request.
+  pauseUntilPlay,
+}
+
+/// An interface for player controllers that allow common basic controls.
+abstract class GenericPlayerController
+    extends ValueNotifier<GenericPlayerValue> {
+  GenericPlayerController({
+    this.obstructionBehavior = ObstructionBehavior.none,
+  }) : super(const GenericPlayerValue(duration: Duration.zero));
+
+  /// Specifies how playback should change when there is some interruption
+  /// to the user.
+  ///
+  /// For example: if a video is hidden because app is in the background,
+  /// or if audio is being interrupted by some music
+  // TODO maybe this should not be part of the interface
+  final ObstructionBehavior obstructionBehavior;
+
+  /// Attempts to open the relevant sources and load metadata about the content.
+  Future<void> initialize();
+
+  @override
+  Future<void> dispose() async => super.dispose();
+
+  /// Starts playing the content.
+  ///
+  /// If the content is at the end, this method starts playing from the beginning.
+  Future<void> play();
+
+  /// Pauses the content.
+  Future<void> pause();
+
+  /// Gets the content's position as a [Duration] from the start.
+  ///
+  /// Will return null if not initialized.
+  Future<Duration?> get position;
+
+  /// Sets the content's current [position].
+  /// The next time the content is played
+  /// it will resume from the given [position].
+  ///
+  /// If [position] is outside of the content's valid range it will
+  /// be automatically and silently clamped.
+  Future<void> setPosition(Duration position);
+
+  /// Sets the playback speed.
+  ///
+  /// [speed] indicates a speed value with different implementations accepting
+  /// different ranges for speed values. The [speed] must be greater than 0.
+  // TODO allow negative speed?
+  Future<void> setPlaybackSpeed(double speed);
+}
+
+/// A group of [GenericPlayerController]s,
+/// which are kept in sync with each other.
+///
+/// All the controllers' play/pause/speed/position states will stay in sync,
+/// while applying their respective offsets.
+///
+/// The group will end playback once the last controller has ended playback.
+///
+/// The total duration will be the largest duration + offset of the controllers.
+class SyncedPlayerControllerGroup extends GenericPlayerController {
+  SyncedPlayerControllerGroup(
+    this.controllers, {
+    this.masterController,
+    this.syncPeriod = const Duration(milliseconds: 500),
+    this.marginOfError = const Duration(milliseconds: 500),
+  });
+
+  /// Map containing all controllers to be kept in sync,
+  /// and their position's offset compared to the group's [position].
+  ///
+  /// If a controller's offset is negative, the start will be cut off.
+  ///
+  /// If this map is modified during playback,
+  /// you can use [forceSync] to update the offsets faster.
+  final Map<GenericPlayerController, Duration> controllers;
+
+  /// A controller to be considered the 'main' reference.
+  /// All [controllers] will be synced relative to this [masterController].
+  ///
+  /// If no [masterController] is specified, sync will be relative to a
+  /// virtual timeline.
+  final GenericPlayerController? masterController;
+
+  /// A duration specifying often should the controllers' position checked and
+  /// synced if needed.
+  final Duration syncPeriod;
+
+  /// A duration specifying how much of a difference can a controller have
+  /// between it's actual position and expected position before being considered
+  /// out of sync.
+  final Duration marginOfError;
+
+  @override
+  Future<void> initialize() async {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> dispose() async {
+    // TODO: implement dispose
+    throw UnimplementedError();
+    super.dispose();
+  }
+
+  @override
+  Future<void> setPosition(Duration position) {
+    // TODO: implement jumpTo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> play() {
+    // TODO: implement play
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> pause() {
+    // TODO: implement pause
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement position
+  Future<Duration?> get position => throw UnimplementedError();
+
+  @override
+  Future<void> setPlaybackSpeed(double speed) {
+    // TODO: implement setPlaybackSpeed
+    throw UnimplementedError();
+  }
+
+  /// Synchronize immediately, instead of waiting for [syncPeriod].
+  Future<void> forceSync({bool ignoreMarginOfError = false}) {
+    // TODO: implement forceSync
+    throw UnimplementedError();
+  }
+
+// TODO volume controls?
+}
