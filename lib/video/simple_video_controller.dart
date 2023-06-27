af41943eee8b7414a5b614400c79a3d69b14e300
McMeck Nechbech <bimpmpbem@gmail.com>
2023-06-27 03:04:48 +0300

    Add `SimpleVideoController`

---
+++ lib/video/simple_video_controller.dart
@@ -0,0 +1,69 @@
+import 'package:video_player/video_player.dart';
+
+import '../synced_player.dart';
+
+extension on VideoPlayerValue {
+  GenericPlayerValue toGenericPlayerValue() => GenericPlayerValue(
+        startPosition: Duration.zero,
+        endPosition: duration,
+        isInitialized: isInitialized,
+        isPlaying: isPlaying,
+        isLooping: isLooping,
+        isBuffering: isBuffering,
+        playbackSpeed: playbackSpeed,
+        errorDescription: errorDescription,
+      );
+}
+
+/// Wrapper for [VideoPlayerController]
+class SimpleVideoController extends GenericPlayerController {
+  SimpleVideoController({
+    super.obstructionBehavior = ObstructionBehavior.none,
+    required this.controller,
+  });
+
+  final VideoPlayerController controller;
+
+  @override
+  Future<void> initialize() async {
+    await controller.initialize();
+    controller.addListener(_listener);
+
+    value = controller.value.toGenericPlayerValue();
+  }
+
+  @override
+  Future<void> dispose() async {
+    controller.removeListener(_listener);
+    await controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Future<void> setPosition(Duration position) => controller.seekTo(position);
+
+  @override
+  Future<void> pause() => controller.pause();
+
+  @override
+  Future<void> play() {
+    return controller.play();
+  }
+
+  @override
+  // TODO check latency
+  Future<Duration?> get position => controller.position;
+
+  @override
+  Future<void> setPlaybackSpeed(double speed) =>
+      controller.setPlaybackSpeed(speed);
+
+  Future<void> _listener() async {
+    // position update manually cause it isn't always up to date
+    // (for example, calling pause() does not update position)
+    // TODO fix video_player?
+    value = controller.value
+        .copyWith(position: await controller.position)
+        .toGenericPlayerValue();
+  }
+}
